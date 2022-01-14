c8e574196cf1a176d0212a0ddc02e3eca92d69e5
Matthew Aylward <matthewtyleraylward@hotmail.com>
2022-01-14 18:05:48 +0100

    ⚙️ Fixed import settings

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,25 +1,47 @@
 from pydantic import BaseSettings
 from pydantic.fields import Field
-from dotenv import load_dotenv
+from pydantic import AnyHttpUrl, BaseSettings, PostgresDsn, validator
+
+from typing import Any, Dict, List, Optional, Union
 
 
 class Settings(BaseSettings):
+    PROJECT_NAME: str
+    BACKEND_CORS_ORIGINS: list[AnyHttpUrl] = []
 
     # Plaid API Settings
     PLAID_CLIENT_ID: str = Field(..., env="PLAID_CLIENT_ID")
     PLAID_SECRET: str = Field(..., env="PLAID_SECRET")
 
-    # Postgres Settings
-    POSTGRES_USER: str = None
-    POSTGRES_PASSWORD: str = None
-    POSTGRES_HOST: str = None
-    POSTGRES_PORT: int = None
-    POSTGRES_DB: str = None
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
 
-    class config:
-        env_file = ".env"
+    POSTGRES_SERVER: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    DATABASE_URI: PostgresDsn | None = None
+
+    @validator("DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_SERVER"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
+
+    class Config:
         case_sensitive = True
-        
+        env_file = ".env"
 
 
 settings = Settings()
